007f1333c22e15374d4d8ceeda1afa4dc2e92959
ValiCoder <Misterivd7@gmail.com>
2025-02-13 21:43:20 +0500

    Initial commit 3

--- src/object/SuperObject.java
+++ src/object/SuperObject.java
@@ -15,6 +15,7 @@ public class SuperObject {
 
     public void draw(Graphics2D g2, GamePanel gp) {
 
+
         int screenX = worldX - gp.player.worldX + gp.player.screenX;
         int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
